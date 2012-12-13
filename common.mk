2e1f676d5dedec91714030fa0555abf77c70a185
Jason Kung <jason.kung@gigabyte.com>
2012-12-13 14:38:56 +0800

    Full language
    Category: system
    
    Change-Id: I41040f1ac776e886535646156de4aa0646f0d360

--- common.mk
+++ common.mk
@@ -555,7 +555,7 @@ $(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-v
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_BRAND := qcom
-PRODUCT_LOCALES := en_US es_US de_DE zh_CN
+PRODUCT_LOCALES := en_US fr_FR it_IT es_ES de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN
 PRODUCT_LOCALES += hdpi mdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
